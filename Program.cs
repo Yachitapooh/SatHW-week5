054c800df4a7a052152caa59e946c4c5be6455c1
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-03-12 00:52:07 +0700

    homework jaaa

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace การบ้านวันเสาร์ครั้งที่5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Xtime, Yprice;
+            string mode;
+
+            Console.WriteLine();
+            mode = Console.ReadLine();
+            Console.WriteLine();
+            Xtime = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Yprice = double.Parse(Console.ReadLine());
+
+            if (mode == "time" || mode == "price")
+            {
+                if (Xtime <= 0)
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+                else if (mode == "time")
+                {
+                    Yprice = Math.Pow((Xtime - 1), 2);
+                    Console.WriteLine("{0}, {1}", Xtime, Yprice);
+                }
+                else if (mode == "price")
+                {
+                    Xtime = Math.Sqrt(Yprice)+1;
+                    Console.WriteLine("{0}, {1}", Xtime, Yprice);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Invalid mode");
+            }
+            Console.ReadLine();
+        }
+    }
+}
